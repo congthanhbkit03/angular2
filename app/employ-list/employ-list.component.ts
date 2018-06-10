4f1f4e29108957ff5b5e3f04a10486b4d4190da5
congthanhbkit03 <congthanhbkit03@gmail.com>
2018-06-10 10:02:24 +0700

    Bai 26

--- app/employ-list/employ-list.component.ts
+++ app/employ-list/employ-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployServiceService} from '../services/employ-service.service';
+import { Router, ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-employ-list',
   templateUrl: './employ-list.component.html',
@@ -7,13 +8,23 @@ import { EmployServiceService} from '../services/employ-service.service';
 })
 export class EmployListComponent implements OnInit {
   public employees: any[];
-  constructor(private employeeService: EmployServiceService) { }
+  public pages: number[];
+  public currentPage: number;
+  constructor(private employeeService: EmployServiceService, private router: Router, private activedRoute: ActivatedRoute) {
+
+  }
 
   ngOnInit() {
+    this.activedRoute.queryParams.subscribe(params => {
+       this.currentPage = params['pageNumber'] || 1;
+       console.log(this.currentPage);
+    });
   	this.employeeService.getList().subscribe((response:any) => {
   		this.employees = response;
-      console.log("AAAAAAAAAAAA" + response);
-  	})
+      // console.log("AAAAAAAAAAAA" + response);
+  	});
+
+    this.pages = [1, 2, 3, 4, 5, 6, 7];
   }
 
 }
